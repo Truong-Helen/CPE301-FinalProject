9f5dd48872651416b9bd73adaca3c56ab74f0b28
Truong-Helen <helen.truong@nevada.unr.edu>
2023-04-30 19:38:57 -0700

    ino file setup

---
+++ cpe301-finalproject/cpe301-finalproject.ino
@@ -0,0 +1,12 @@
+// Team: Helen Truong
+// CPE301 - Final Project
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
